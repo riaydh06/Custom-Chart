2b8a18b5c4aab67193f09d928807516fc4b5cb5e
Sayed Riaydh <riaydh06@gmail.com>
2024-05-03 12:20:49 +0600

    feat: barChart added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BarChart from "./graph/BarChart";
 
+const data = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  data: [12, 14, 11, 14, 15, 2, 13],
+  backgroundColor: [
+    "rgba(255, 99, 132, 0.2)",
+    "rgba(255, 159, 64, 0.2)",
+    "rgba(255, 205, 86, 0.2)",
+    "rgba(75, 192, 192, 0.2)",
+    "rgba(54, 162, 235, 0.2)",
+    "rgba(153, 102, 255, 0.2)",
+    "rgba(201, 203, 207, 0.2)",
+  ],
+  borderColor: [
+    "rgb(255, 99, 132)",
+    "rgb(255, 159, 64)",
+    "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)",
+    "rgb(54, 162, 235)",
+    "rgb(153, 102, 255)",
+    "rgb(201, 203, 207)",
+  ],
+  borderWidth: 1,
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BarChart configs={data} />;
 }
 
 export default App;
---
+++ src/graph/BarChart.tsx
@@ -0,0 +1,106 @@
+const SVG_WIDTH = 600;
+const SVG_HEIGHT = 300;
+
+interface Props {
+  configs: {
+    labels: string[];
+    data: number[];
+    backgroundColor?: string[];
+    borderColor?: string[];
+    borderWidth?: number;
+  };
+}
+
+function BarChart({ configs }: Props) {
+  const { labels, data, backgroundColor, borderColor, borderWidth } = configs;
+  const x0 = 50;
+  const xAxisLength = SVG_WIDTH - x0 * 2;
+
+  const y0 = 50;
+  const yAxisLength = SVG_HEIGHT - y0 * 2;
+
+  const xAxisY = y0 + yAxisLength;
+
+  const dataYMax = data.reduce(
+    (currMax, dataY) => Math.max(currMax, dataY),
+    -Infinity
+  );
+  const dataYMin = data.reduce(
+    (currMin, dataY) => Math.min(currMin, dataY),
+    Infinity
+  );
+  const dataYRange = dataYMax - dataYMin;
+
+  const numYTicks = 5;
+
+  const barPlotWidth = xAxisLength / data.length;
+
+  return (
+    <svg width={SVG_WIDTH} height={SVG_HEIGHT}>
+      {/* X axis */}
+      <line
+        x1={x0}
+        y1={xAxisY}
+        x2={x0 + xAxisLength}
+        y2={xAxisY}
+        stroke="grey"
+      />
+      <text x={x0 + xAxisLength + 5} y={xAxisY + 4}>
+        Day
+      </text>
+
+      {/* Y axis */}
+      <line x1={x0} y1={y0} x2={x0} y2={y0 + yAxisLength} stroke="grey" />
+      {Array.from({ length: numYTicks }).map((_, index) => {
+        const y = y0 + index * (yAxisLength / numYTicks);
+
+        const yValue = Math.round(dataYMax - index * (dataYRange / numYTicks));
+
+        return (
+          <g key={index}>
+            <line x1={x0} y1={y} x2={x0 - 5} y2={y} stroke="grey" />
+            <text x={x0 - 5} y={y + 5} textAnchor="end">
+              {yValue}
+            </text>
+          </g>
+        );
+      })}
+      <text x={x0} y={y0 - 8} textAnchor="middle">
+        $
+      </text>
+
+      {/* Bar plots */}
+      {labels.map((label, index) => {
+        const x = x0 + index * barPlotWidth;
+
+        const yRatio = (data[index] - dataYMin) / dataYRange;
+
+        const y = y0 + (1 - yRatio) * yAxisLength;
+        const height = yRatio * yAxisLength;
+
+        const sidePadding = 10;
+
+        console.log({ x, yRatio, y, height, dd: data[index] });
+
+        return (
+          <g key={index}>
+            <rect
+              x={x + sidePadding / 2}
+              y={y}
+              width={barPlotWidth - sidePadding}
+              height={height}
+              fill={backgroundColor?.[index] || "black"}
+              stroke={borderColor?.[index] || "black"}
+              strokeWidth={borderWidth || 1}
+            />
+            <text x={x + barPlotWidth / 2} y={xAxisY + 16} textAnchor="middle">
+              {label}
+            </text>
+          </g>
+        );
+      })}
+    </svg>
+  );
+}
+
+export default BarChart;
